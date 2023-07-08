d6fa76f563ec5b63c2c64256f2dcd4a1a16623a2
Tatiana Slonovskaya <tatianaslonovskaya@gmail.com>
2023-07-08 21:50:02 +0200

    Solved problems for lesson 3 part 4

--- Lesson3/Part3/Array32/Array32.cpp
+++ Lesson3/Part3/Array32/Array32.cpp
@@ -2,23 +2,15 @@
 using namespace std;
 
 int main() {
-    int array_size;
-    int array[array_size];
-    
-    cout << "Enter the size of an array: " << endl;
-    cin >> array_size;
-    
-    cout << endl;
-    cout << "Enter the numbers: " << endl;
-    for (int i = 0; i < array_size; i++) {
-    	cin >> array[1];
-	}
-	
-	cout << endl;
-	for (int i = 1; i < array_size - 1; i++) {
-        if (array[i] < array[i - 1] && array[i] < array[i + 1]) {
-        	cout << i << endl;
-		}
-	cout << "Not found!" << endl;
-    }
+   int num = 4;
+   int num2 = 10;
+   int* pointer = &num;
+   
+   cout << "I'm just a number " << num << endl;
+   cout << "I'm an address where " << num << " is located: " << pointer << endl;
+   cout << "I'm the adress of the pointer itself: " << &pointer;
+   
+   pointer = &num2;
+   cout << endl << "I'm now pointing to " << num2 << " and my adress is now " << pointer;
+   
 }
